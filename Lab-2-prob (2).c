10f3172e771e6b97cadd2b0ef7e89152f8e84b93
raiyan025 <raiyanchoudhury8@gmail.com>
2025-07-12 21:18:32 +0600

    Update Lab-2-prob (2).c

--- Lab-2-prob (2).c
+++ Lab-2-prob (2).c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main()
 {
@@ -19,5 +19,34 @@ int main()
     {
         printf("The number is 0");
     }
+    return 0;
+}*/
+
+
+// Nested if else
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Input Number:");
+    scanf("%d", &a);
+    if (a != 0)
+    {
+        if (a > 0)
+        {
+            printf("%d is positive", a);
+        }
+        else
+        {
+            printf("%d is negative", a);
+        }
+    }
+    else
+    {
+        printf("The number is zero");
+    }
+
     return 0;
 }
