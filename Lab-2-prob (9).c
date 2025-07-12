dbc00681f15424766c16c23e224ab631ff1656df
raiyan025 <raiyanchoudhury8@gmail.com>
2025-07-12 22:21:37 +0600

    Update Lab-2-prob (9).c

--- Lab-2-prob (9).c
+++ Lab-2-prob (9).c
@@ -37,3 +37,40 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+// extra
+
+/*#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("Enter x and y coordinates: ");
+    scanf("%d %d", &x, &y);
+
+    if (x > 0 && y > 0) {
+        printf("(%d, %d) point is on the 1st quadrant.\n", x, y);
+    } else if (x < 0 && y > 0) {
+        printf("(%d, %d) point is on the 2nd quadrant.\n", x, y);
+
+        // Check if on the rectangle inside 2nd quadrant
+        if (x >= -4 && x <= -1 && y >= 1 && y <= 4) {
+            printf("It is on the Rectangle.\n");
+        } else {
+            printf("It is not on the Rectangle.\n");
+        }
+
+    } else if (x < 0 && y < 0) {
+        printf("(%d, %d) point is on the 3rd quadrant.\n", x, y);
+    } else if (x > 0 && y < 0) {
+        printf("(%d, %d) point is on the 4th quadrant.\n", x, y);
+    } else {
+        printf("(%d, %d) point is on the axis.\n", x, y);
+    }
+
+    return 0;
+}
+*/
