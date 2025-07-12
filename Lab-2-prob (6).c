9760e9371fda4c615858403fc0257f5d2b1fa96d
raiyan025 <raiyanchoudhury8@gmail.com>
2025-07-12 21:45:59 +0600

    Update Lab-2-prob (6).c

--- Lab-2-prob (6).c
+++ Lab-2-prob (6).c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main()
 {
@@ -26,6 +26,51 @@ int main()
     {
 
         printf("There is no maximum number");
+    }
+
+    return 0;
+}*/
+
+
+// Nested if-else
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b, c;
+    printf("Input num1:", a);
+    scanf("%d", &a);
+    printf("Input num2:", b);
+    scanf("%d", &b);
+    printf("Input num3:", c);
+    scanf("%d", &c);
+    if (a > b)
+    {
+        if (a > c)
+        {
+            printf("Maximum:%d", a);
+        }
+
+        else if (c > a)
+        {
+            printf("Maximum:%d", c);
+        }
+        else
+        {
+
+            printf("There is no maximum number"); // a == c and both > b
+        }
+    }
+    else
+    {
+        if (b > c)
+            printf("Maximum: %d", b);
+        else if (c > b)
+            printf("Maximum: %d", c);
+        else
+            printf("There is no maximum number"); // b == c and both > a
     }
 
     return 0;
